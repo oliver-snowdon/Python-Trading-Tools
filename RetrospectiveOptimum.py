a842b994de6d4aae047f32f9bc77fe733146621e
Oliver Snowdon <oliver.snowdon@cantab.net>
2019-03-12 08:31:48 +0000

    Make RetrospectiveOptimum not say anything about the final data point, because it cannot predict the future

--- RetrospectiveOptimum.py
+++ RetrospectiveOptimum.py
@@ -16,5 +16,5 @@ def RetrospectiveOptimum(ask, bid, exchangeFee, nSamples):
 		Qs[sellIfCan, i] = np.log(1-exchangeFee) + max(Qs[keepFiatIfCan, i+1], Qs[buyIfCan, i+1])
 		Qs[keepAssetIfCan, i] = np.log(bid[i+1]/bid[i]) + max(Qs[keepAssetIfCan, i+1], Qs[sellIfCan, i+1])
 
-	return np.argmax(Qs[0:2], axis=0), np.argmax(Qs[2:4], axis=0)
+	return np.argmax(Qs[0:2, 0:nSamples-1], axis=0), np.argmax(Qs[2:4, 0:nSamples-1], axis=0)
 
