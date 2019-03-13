2188b2e5c75abd3933720045b03dd0db3c198590
Oliver Snowdon <oliver.snowdon@cantab.net>
2019-03-13 17:14:36 +0000

    Fixup

--- Logger.py
+++ Logger.py
@@ -1,17 +1,18 @@
 import asyncio
 from Kraken import WebsocketLogger
 from Database import Database
+import time
 
 pairs = ["XBT/EUR", "ETH/EUR"]
 
 while True:
 	try:
 		database = Database()
+		pairIds = database.GetPairIds(pairs)
+		print(pairIds)
 		break
 	except Exception as error:
 		print('Caught this error while opening database: ' + repr(error))
 		time.sleep(3)
 
-pairIds = database.GetPairIds(pairs)
-print(pairIds)
 asyncio.get_event_loop().run_until_complete(WebsocketLogger.Stream(pairs, database, pairIds))
