06885ae4651d0ec084d16e1fabcd02192e8ba63f
Oliver Snowdon <oliver.snowdon@cantab.net>
2019-03-20 07:24:36 +0000

    Add TimerThread

---
+++ TimerThread.py
@@ -0,0 +1,26 @@
+import datetime, threading, time
+
+class TimerThread:
+
+	def __init__(self, nSecondsBetweenCall):
+		self.thread = threading.Thread(target=self.Loop)
+		self.nSecondsBetweenCall = nSecondsBetweenCall
+		self.keepRunning = False
+
+	def Loop(self):
+		nextCall = time.time()
+		while self.keepRunning:
+			nextCall = nextCall + self.nSecondsBetweenCall
+			self.Target()
+			time.sleep(nextCall - time.time())
+
+	def Target(self):
+		print (datetime.datetime.now())
+
+	def Start(self):
+		self.keepRunning = True
+		self.thread.start()
+
+if __name__ == '__main__':
+	timerThread = TimerThread(1)
+	timerThread.Start()
